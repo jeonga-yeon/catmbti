90ce8aabd47590b90580ebd00ee710b640f5c066
joyfuldev <wings2132@naver.com>
2022-09-08 21:34:53 +0900

    라우트 로직

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,6 +2,7 @@ import React from "react";
 import styled from "styled-components";
 import HomeImage from "../assets/cat.jpg";
 import Button from "react-bootstrap/Button";
+import { useNavigate } from "react-router-dom";
 
 const Wrapper = styled.div`
   height: 100vh;
@@ -13,6 +14,7 @@ const Header = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
+  font-family: "SimKyungha";
 `;
 
 const Contents = styled.div`
@@ -25,6 +27,7 @@ const Contents = styled.div`
 const Title = styled.div`
   font-size: 30pt;
   margin-top: 40px;
+  font-family: "SimKyungha";
 `;
 
 const LogoImage = styled.div`
@@ -34,9 +37,14 @@ const LogoImage = styled.div`
 const Desc = styled.div`
   font-size: 20pt;
   margin-top: 20px;
+  font-family: "SimKyungha";
 `;
 
 const Home = () => {
+  const navigate = useNavigate();
+  const handleClickBtn = () => {
+    navigate("/question");
+  };
   return (
     <Wrapper>
       <Header>예비집사 판별기</Header>
@@ -52,7 +60,9 @@ const Home = () => {
           />
         </LogoImage>
         <Desc>MBTI를 기반으로 하는 나랑 잘맞는 고양이 찾기!</Desc>
-        <Button>테스트 시작하기</Button>
+        <Button style={{ fontFamily: "SimKyungha" }} onClick={handleClickBtn}>
+          테스트 시작하기
+        </Button>
       </Contents>
     </Wrapper>
   );
